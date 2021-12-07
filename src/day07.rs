305cb0967978a2fcedc134aa2972e6cb8a4a5b4f
Dan Nguyen <pedantic@gmail.com>
2021-12-07 00:29:58 -0600

    (day7) Refactor to use usize

--- src/day07.rs
+++ src/day07.rs
@@ -1,40 +1,46 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
-const TRI: [isize; 2048] = precalculate_triangle_seq();
+const TRI: [usize; 2048] = precalculate_triangle_seq();
 
-const fn precalculate_triangle_seq<const N: usize>() -> [isize; N] {
+const fn precalculate_triangle_seq<const N: usize>() -> [usize; N] {
     let mut res = [0; N];
 
     let mut i = 0;
     while i < N {
-        let j = i as isize;
-        res[i] = j * (j + 1) / 2;
+        res[i] = i * (i + 1) / 2;
         i += 1;
     }
 
     res
 }
 
+#[inline]
+fn sub_abs(a: usize, b: usize) -> usize {
+    let a = a as isize;
+    let b = b as isize;
+    (a - b).abs() as usize
+}
+
 #[aoc_generator(day7)]
-pub fn generator(input: &str) -> Vec<isize> {
-    let mut v: Vec<isize> = input.split(',').map(|x| x.parse().unwrap()).collect();
+pub fn generator(input: &str) -> Vec<usize> {
+    let mut v: Vec<usize> = input.split(',').map(|x| x.parse().unwrap()).collect();
     v.sort_unstable();
     v
 }
 
 #[aoc(day7, part1)]
-pub fn part1(inputs: &[isize]) -> isize {
+pub fn part1(inputs: &[usize]) -> usize {
     let median = inputs[inputs.len() / 2];
 
-    inputs.iter().map(|x| (x - median).abs()).sum()
+    inputs.iter().map(|&x| sub_abs(x, median)).sum()
 }
 
 #[aoc(day7, part2)]
-pub fn part2(inputs: &[isize]) -> isize {
-    let mean = inputs.iter().sum::<isize>() / inputs.len() as isize;
+pub fn part2(inputs: &[usize]) -> usize {
+    let mean: usize = inputs.iter().sum::<usize>() / inputs.len();
 
     (mean..=(mean + 1))
-        .map(|n| inputs.iter().map(|x| TRI[(x - n).abs() as usize]).sum())
+        .map(|n| inputs.iter().map(|&x| TRI[sub_abs(x, n)]).sum())
         .min()
         .unwrap()
 }
@@ -66,7 +72,7 @@ mod tests {
         use super::*;
 
         const INPUT: &str = include_str!("../input/2021/day7.txt");
-        const ANSWERS: (isize, isize) = (352997, 101571302);
+        const ANSWERS: (usize, usize) = (352997, 101571302);
 
         #[test]
         pub fn test() {
