2a9c9419e2be1b0e4b3f53e301f760f8997b4581
Dan Nguyen <pedantic@gmail.com>
2021-12-09 08:48:02 -0600

    (day9) refactor neighbors again, remove for loops

--- src/day09.rs
+++ src/day09.rs
@@ -1,4 +1,5 @@
-use std::{cmp::Reverse, iter::from_fn};
+use crate::utils::IsizeAdd;
+use std::cmp::Reverse;
 
 use aoc_runner_derive::{aoc, aoc_generator};
 
@@ -11,39 +12,14 @@ pub fn generator(input: &str) -> Vec<Vec<u8>> {
 }
 
 fn neighbors(r: usize, c: usize, r_m: usize, c_m: usize) -> impl Iterator<Item = (usize, usize)> {
-    let mut kind = 0;
+    [(-1, 0), (0, -1), (0, 1), (1, 0)]
+        .iter()
+        .filter_map(move |&(y, x)| {
+            let r_new = r.isize_add(y)?;
+            let c_new = c.isize_add(x)?;
 
-    from_fn(move || {
-        if kind == 0 {
-            kind += 1;
-            if r > 0 {
-                return Some((r - 1, c));
-            }
-        }
-
-        if kind == 1 {
-            kind += 1;
-            if c > 0 {
-                return Some((r, c - 1));
-            }
-        }
-
-        if kind == 2 {
-            kind += 1;
-            if c + 1 < c_m {
-                return Some((r, c + 1));
-            }
-        }
-
-        if kind == 3 {
-            kind += 1;
-            if r + 1 < r_m {
-                return Some((r + 1, c));
-            }
-        }
-
-        None
-    })
+            Some((r_new, c_new)).filter(|_| r_new < r_m && c_new < c_m)
+        })
 }
 
 #[aoc(day9, part1)]
@@ -69,29 +45,25 @@ pub fn part2(inputs: &[Vec<u8>]) -> usize {
     let r_max = inputs.len();
     let c_max = inputs[0].len();
     let mut v = inputs.to_vec();
-    let mut areas = Vec::new();
-
-    for (row, col) in (0..r_max).flat_map(|row| (0..c_max).map(move |col| (row, col))) {
-        if v[row][col] < 9 {
-            areas.push(find_basin(&mut v, row, col, r_max, c_max));
-        }
-    }
+    let mut areas: Vec<_> = (0..r_max)
+        .flat_map(|row| (0..c_max).map(move |col| (row, col)))
+        .map(|(row, col)| find_basin(&mut v, row, col, r_max, c_max))
+        .collect();
 
     areas.sort_by_key(|&x| Reverse(x));
     areas[0..3].iter().product()
 }
 
 fn find_basin(v: &mut Vec<Vec<u8>>, row: usize, col: usize, r_max: usize, c_max: usize) -> usize {
-    let mut total = 1;
-    v[row][col] = 10;
-
-    for (r, c) in neighbors(row, col, r_max, c_max) {
-        if v[r][c] < 9 {
-            total += find_basin(v, r, c, r_max, c_max);
-        }
+    if v[row][col] > 8 {
+        return 0;
     }
 
-    total
+    v[row][col] = 10;
+
+    1 + neighbors(row, col, r_max, c_max)
+        .map(|(r, c)| find_basin(v, r, c, r_max, c_max))
+        .sum::<usize>()
 }
 
 #[cfg(test)]
