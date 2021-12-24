3bfaa1f804e9343ec35b5b3e09662728b2a94d96
Dan Nguyen <pedantic@gmail.com>
2021-12-24 09:42:41 -0600

    (day24) Refactor to calculate answer directly

--- src/day24.rs
+++ src/day24.rs
@@ -1,210 +1,65 @@
-use std::{convert::Infallible, str::FromStr};
-
 use aoc_runner_derive::{aoc, aoc_generator};
-use rustc_hash::FxHashMap;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-enum Variable {
-    Number(isize),
-    RegisterX,
-    RegisterY,
-    RegisterZ,
-    RegisterW,
-}
-
-impl FromStr for Variable {
-    type Err = Infallible;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(match s {
-            "w" => Variable::RegisterW,
-            "x" => Variable::RegisterX,
-            "y" => Variable::RegisterY,
-            "z" => Variable::RegisterZ,
-            _ => Variable::Number(s.parse().unwrap()),
-        })
-    }
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-enum Instruction {
-    Inp(Variable),
-    Add(Variable, Variable),
-    Mul(Variable, Variable),
-    Div(Variable, Variable),
-    Mod(Variable, Variable),
-    Eql(Variable, Variable),
-}
-
-impl FromStr for Instruction {
-    type Err = Infallible;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (ins, variables) = s.split_once(' ').unwrap();
-        if s.starts_with("inp") {
-            Ok(Instruction::Inp(variables.parse().unwrap()))
-        } else {
-            let (a, b) = variables.split_once(' ').unwrap();
-            let a = a.parse().unwrap();
-            let b = b.parse().unwrap();
-            Ok(match ins {
-                "add" => Instruction::Add(a, b),
-                "mul" => Instruction::Mul(a, b),
-                "div" => Instruction::Div(a, b),
-                "mod" => Instruction::Mod(a, b),
-                "eql" => Instruction::Eql(a, b),
-                _ => panic!("invalid instruction: {}", ins),
-            })
-        }
-    }
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Default, Hash)]
-struct Alu {
-    x: isize,
-    y: isize,
-    z: isize,
-    w: isize,
-}
-
-impl Alu {
-    fn get_variable(&self, variable: Variable) -> isize {
-        match variable {
-            Variable::Number(x) => x,
-            Variable::RegisterX => self.x,
-            Variable::RegisterY => self.y,
-            Variable::RegisterZ => self.z,
-            Variable::RegisterW => self.w,
-        }
-    }
-
-    fn set_variable(&mut self, variable: Variable, value: isize) {
-        match variable {
-            Variable::Number(_) => panic!("you cannot set a Number"),
-            Variable::RegisterX => self.x = value,
-            Variable::RegisterY => self.y = value,
-            Variable::RegisterZ => self.z = value,
-            Variable::RegisterW => self.w = value,
-        }
-    }
-
-    fn is_valid(&self) -> bool {
-        self.z == 0
-    }
-
-    fn process_ins(&mut self, ins: &Instruction, input: usize) {
-        match ins {
-            Instruction::Inp(v) => {
-                self.set_variable(*v, input as isize);
-            }
-            Instruction::Add(a, b) => {
-                let x = self.get_variable(*a);
-                let y = self.get_variable(*b);
-                self.set_variable(*a, x + y);
-            }
-            Instruction::Mul(a, b) => {
-                let x = self.get_variable(*a);
-                let y = self.get_variable(*b);
-                self.set_variable(*a, x * y);
-            }
-            Instruction::Div(a, b) => {
-                let x = self.get_variable(*a);
-                let y = self.get_variable(*b);
-                self.set_variable(*a, x / y);
-            }
-            Instruction::Mod(a, b) => {
-                let x = self.get_variable(*a);
-                let y = self.get_variable(*b);
-                self.set_variable(*a, x % y);
-            }
-            Instruction::Eql(a, b) => {
-                let x = self.get_variable(*a);
-                let y = self.get_variable(*b);
-                self.set_variable(*a, if x == y { 1 } else { 0 });
-            }
-        }
-    }
+fn parse_instruction(s: &str) -> isize {
+    s.rsplit_once(' ').unwrap().1.parse().unwrap()
 }
 
 #[derive(Debug, PartialEq)]
 pub struct Day24 {
-    ins: Vec<Instruction>,
+    instructions: Vec<[isize; 3]>,
 }
 
 #[aoc_generator(day24)]
 pub fn generator(input: &str) -> Day24 {
-    Day24 {
-        ins: input.lines().map(|l| l.parse().unwrap()).collect(),
-    }
+    let ins = input
+        .split("\ninp")
+        .map(|chunk| {
+            let mut itr = chunk.lines();
+
+            // get every 4th, 5th, and 15th instruction after an "inp"
+            [
+                parse_instruction(itr.nth(4).unwrap()),
+                parse_instruction(itr.next().unwrap()),
+                parse_instruction(itr.nth(9).unwrap()),
+            ]
+        })
+        .collect();
+
+    Day24 { instructions: ins }
 }
 
-fn solve(
-    program: &[Instruction],
-    pc: usize,
-    alu: Alu,
-    digits: &[usize],
-    seen: &mut FxHashMap<(Alu, usize), Option<usize>>,
-    acc: usize,
-) -> Option<usize> {
-    if let Some(ans) = seen.get(&(alu, pc)) {
-        return *ans;
-    }
+fn solve(instructions: &[[isize; 3]]) -> (usize, usize) {
+    let mut stack = Vec::new();
+    let mut max = [0; 14];
+    let mut min = [0; 14];
 
-    'inputs: for &input in digits {
-        let mut alu = alu;
-        let mut pc = pc;
-        alu.process_ins(&program[pc], input);
-        pc += 1;
+    for (i, &[div_x, add_x, add_y]) in instructions.iter().enumerate() {
+        if div_x == 1 {
+            stack.push((i, add_y));
+        } else if let Some((j, ins_y)) = stack.pop() {
+            let delta = ins_y + add_x;
 
-        while let Some(inst) = program.get(pc) {
-            if let Instruction::Inp(_) = inst {
-                if let Some(ans) = solve(program, pc, alu, digits, seen, acc * 10 + input) {
-                    seen.insert((alu, pc), Some(ans));
-                    return Some(ans);
-                } else {
-                    continue 'inputs;
-                }
-            } else {
-                alu.process_ins(inst, input);
-                pc += 1;
-            }
-        }
-
-        if alu.is_valid() {
-            let total = Some(acc * 10 + input);
-            seen.insert((alu, pc), total);
-            return total;
+            max[i] = 9.min(9 + delta);
+            max[j] = 9.min(9 - delta);
+            min[i] = 1.max(1 + delta);
+            min[j] = 1.max(1 - delta);
         }
     }
 
-    seen.insert((alu, pc), None);
-    None
+    (
+        min.iter().fold(0, |acc, &d| acc * 10 + d as usize),
+        max.iter().fold(0, |acc, &d| acc * 10 + d as usize),
+    )
 }
 
 #[aoc(day24, part1)]
 pub fn part1(instructions: &Day24) -> usize {
-    solve(
-        &instructions.ins,
-        0,
-        Alu::default(),
-        &[9, 8, 7, 6, 5, 3, 1],
-        &mut FxHashMap::default(),
-        0,
-    )
-    .unwrap()
+    solve(&instructions.instructions).1
 }
 
 #[aoc(day24, part2)]
 pub fn part2(instructions: &Day24) -> usize {
-    solve(
-        &instructions.ins,
-        0,
-        Alu::default(),
-        &[1, 2, 7, 3, 4, 9],
-        &mut FxHashMap::default(),
-        0,
-    )
-    .unwrap()
+    solve(&instructions.instructions).0
 }
 
 #[cfg(test)]
