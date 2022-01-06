60f5fe7a0f91ab4a76160c7a0f61667c6d7e2a18
Dan Nguyen <pedantic@gmail.com>
2022-01-06 11:52:38 -0600

    (infra) Support running individual days with time-all.sh

--- time-all.sh
+++ time-all.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-hyperfine --warmup 2 'cargo test --release --quiet --lib -- --test-threads=1 regression'
+if [ -n "$1" ]; then
+    tests=$(printf "day%02d::tests::regression" $1)
+else
+    tests="regression"
+fi
+
+hyperfine --warmup 2 "cargo test --release --quiet --lib -- --test-threads=1 $tests"
